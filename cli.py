87cd7ade0a31e25c5256b74fff6f71473f713d4f
fedja <fedjamladenovic@gmail.com>
2023-05-15 22:36:10 +0200

    pomeren fajl radi testiranja iz CLI

--- cli.py
+++ cli.py
@@ -4,7 +4,11 @@ from backend.ring import populate, keyrings, Keyring
 from backend.messages import create_message, read_message, send_message, receive_message
 from backend.utils import AsymEnc, SymEnc
 
+from tkinter import Tk
+
 if __name__ == '__main__':
+    root = Tk()
+
     populate()
     print(str(keyrings["fedja"]))
     print('////////////////////////////////////////////////////////////\n')
@@ -12,6 +16,7 @@ if __name__ == '__main__':
     print('////////////////////////////////////////////////////////////\n')
     print(Keyring.all_public_keys())
 
+    # keyrings["fedja"].private[0].export_key("key1")
 
     msg = create_message("zdravo fedja", auth=keyrings["fedja"].private[0], encr=(Keyring.public[2], SymEnc.DES3), compr=True, radix64=True)
 
